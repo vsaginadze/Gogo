c97756c71c7e7a77e9bf62cf6a8081faa484bf60
vsaginadze <vakhtang_saginadze@protonmail.com>
2025-03-31 20:14:15 +0400

    added namespace and cgroups

--- container/main.go
+++ container/main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
 	"os/exec"
+	"path/filepath"
+	"strconv"
 	"syscall"
 )
 
@@ -13,6 +16,8 @@ func main() {
 	switch os.Args[1] {
 	case "run":
 		run()
+	case "child":
+		child()
 	default:
 		panic("what?")
 	}
@@ -20,18 +25,51 @@ func main() {
 }
 
 func run() {
-	fmt.Printf("Running %v\n", os.Args[2:])
+	fmt.Printf("Running %v as %d \n", os.Args[2:], os.Getpid())
+
+	cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	//
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Cloneflags:   syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS,
+		Unshareflags: syscall.CLONE_NEWNS,
+	}
+
+	cmd.Run()
+}
+
+func child() {
+	fmt.Printf("Running %v as %d \n", os.Args[2:], os.Getpid())
+	cg()
+	syscall.Sethostname([]byte("amazon-linux"))
+	syscall.Chroot("/home/ec2-user/workdir")
+	syscall.Chdir("/")
+	syscall.Mount("proc", "proc", "proc", 0, "")
 
 	cmd := exec.Command(os.Args[2], os.Args[3:]...)
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 
-	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Cloneflags: syscall.CLONE_NEWUTS,
+	cmd.Run()
+
+	syscall.Unmount("/proc", 0)
+}
+
+func cg() {
+	cgroups := "/sys/fs/cgroup"
+	pids := filepath.Join(cgroups, "pids")
+	err := os.Mkdir(filepath.Join(pids, "test"), 0755)
+	if err != nil && !os.IsExist(err) {
+		panic(err)
 	}
 
-	cmd.Run()
+	must(ioutil.WriteFile(filepath.Join(pids, "test/pids.max"), []byte("10"), 0700))
+	must(ioutil.WriteFile(filepath.Join(pids, "test/cgroup.procs"), []byte(strconv.Itoa(os.Getpid())), 0700))
+	must(ioutil.WriteFile(filepath.Join(pids, "test/notify_on_release"), []byte("1"), 0700))
 }
 
 func must(err error) {
@@ -39,3 +77,4 @@ func must(err error) {
 		panic(err)
 	}
 }
+
